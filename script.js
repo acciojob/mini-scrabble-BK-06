e47dc0b4f317dd0579795729d5dee7c66392fc4b
Neon <105565447+BK-06@users.noreply.github.com>
2023-01-10 20:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+"use strict";
+
+const input = document.getElementById("evaluatedText");
+const counter = document.getElementById("letterCount");
+
+input.addEventListener("keyup", getLength);
+
+function getLength() {
+  const value = (input.value || "").length;
+  counter.textContent = value;
+}
+getLength();
